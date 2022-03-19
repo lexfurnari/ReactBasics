dc2db30a2f003f15b6a27a06a14173fa99109773
Alexa Furnari <alexa.furnari@nationalgrid.com>
2022-03-19 14:39:20 -0400

    update notes

--- index.js
+++
@@ -1,9 +0,0 @@
-/* Surprise! You probably thought you could just forget
-the line of code you just learned! Nope, not on my watch!
-
-Try to write that 1-liner of React code again! This time,
-see if you can figure out how to render an <ul> with 2+ <li>s inside*/
-ReactDOM.render(
-    <ul><li>Bob</li><li>Mackenzie</li></ul>,
-    document.getElementById("root")
-)
